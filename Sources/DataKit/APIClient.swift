7c8a7e54268674016cacb0e353f6a28ee3b93acb
Alex Du Bois <alex.dubois@mac.com>
2019-11-03 09:28:57 -0600

    Add APIClient

---
+++ Sources/DataKit/APIClient.swift
@@ -0,0 +1,91 @@
+import Foundation
+import Combine
+
+public protocol APIClient {
+    var session: URLSession { get }
+    var configuration: URLSessionConfiguration { get }
+    
+    var cachePolicy: URLRequest.CachePolicy { get }
+    var timeout: TimeInterval { get }
+    var customHeaders: [String: String] { get }
+}
+
+public extension APIClient {
+    func publisher(_ request: URLRequest) -> AnyPublisher<HTTPOutput, URLError> {
+        return session.dataTaskPublisher(for: request)
+            .map { (data, response) in
+                return (data, response as! HTTPURLResponse)
+        }.eraseToAnyPublisher()
+    }
+    
+    func publisher(_ url: URL) -> AnyPublisher<HTTPOutput, URLError> {
+        return session.dataTaskPublisher(for: url)
+            .map { (data, response) in
+                return (data, response as! HTTPURLResponse)
+        }.eraseToAnyPublisher()
+    }
+    
+    func newRequest(_ url: URL,
+                 cachePolicy: URLRequest.CachePolicy? = nil,
+                 timeoutInterval: TimeInterval? = nil,
+                 headers: [String: String] = [:]) -> URLRequest {
+        return URLRequest(url: url,
+                          cachePolicy: cachePolicy ?? self.cachePolicy,
+                          timeoutInterval: timeoutInterval ?? self.timeout)
+            .appending(headers: customHeaders.merging(headers,
+                                                      uniquingKeysWith: { return $1 }))
+    }
+}
+
+extension APIClient {
+    func execute(_ request: URLRequest) -> Future<HTTPOutput, URLError> {
+        return Future { promise in
+            let _ = self.publisher(request)
+                .sink(receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        return promise(.failure(error))
+                    }
+                }) { value in
+                    return promise(.success(value))
+            }
+        }
+    }
+}
+
+public extension APIClient {
+    func get(_ url: URL) -> AnyPublisher<HTTPOutput, URLError> {
+        let request = newRequest(url)
+        return get(request)
+    }
+
+    func get(_ request: URLRequest) -> AnyPublisher<HTTPOutput, URLError> {
+        var request = request.appending(headers: customHeaders)
+        request.httpMethod = "GET"
+        return execute(request).eraseToAnyPublisher()
+    }
+
+    func post(_ url: URL) -> AnyPublisher<HTTPOutput, URLError> {
+        let request = newRequest(url)
+        return post(request)
+    }
+
+    func post(_ request: URLRequest) -> AnyPublisher<HTTPOutput, URLError> {
+        var request = request.appending(headers: customHeaders)
+        request.httpMethod = "POST"
+        return execute(request).eraseToAnyPublisher()
+    }
+}
+
+public extension APIClient {
+    typealias HTTPOutput = (data: Data, response: HTTPURLResponse)
+}
+
+extension URLRequest {
+    func appending(headers: [String: String]) -> URLRequest {
+        var request = self
+        for (_, (key, value)) in headers.enumerated() {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        return request
+    }
+}
