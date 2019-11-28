647a4c0eca15d6c2c2847ce4b56996a60b0f31c1
Alex <alextall@users.noreply.github.com>
2019-11-27 18:25:01 -0600

    Refactor HTTPOutput as HTTPResponse

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -32,7 +32,7 @@ public enum HTTPError: Error {
 extension HTTPClient {
     /// Execute the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func execute(_ request: URLRequest) -> Future<HTTPOutput, HTTPError> {
+    func execute(_ request: URLRequest) -> Future<HTTPResponse, HTTPError> {
         return Future { promise in
             self.session.dataTask(with: request,
                                   completionHandler: { data, response, error in
@@ -41,9 +41,11 @@ extension HTTPClient {
                                       } else if let error = error {
                                           promise(.failure(.unknown(error)))
                                       }
-                                      let data = data ?? Data()
                                       let response = response as! HTTPURLResponse
-                                      promise(.success(.init(data: data, response: response)))
+                                      if let data = data {
+                                        promise(.success(.body(data: data, response: response)))
+                                      }
+                                      promise(.success(.empty(response)))
             }).resume()
         }
     }
@@ -54,14 +56,14 @@ extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `GET` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func get(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func get(_ url: URL) -> AnyPublisher<HTTPResponse, HTTPError> {
         let request = newRequest(url)
         return get(request)
     }
 
     /// Execute a `GET` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func get(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func get(_ request: URLRequest) -> AnyPublisher<HTTPResponse, HTTPError> {
         var request = request.appending(headers: customHeaders)
         request.httpMethod = "GET"
         return execute(request).eraseToAnyPublisher()
@@ -73,14 +75,14 @@ public extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `POST` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func post(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func post(_ url: URL) -> AnyPublisher<HTTPResponse, HTTPError> {
         let request = newRequest(url)
         return post(request)
     }
 
     /// Execute a `POST` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func post(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func post(_ request: URLRequest) -> AnyPublisher<HTTPResponse, HTTPError> {
         var request = request.appending(headers: customHeaders)
         request.httpMethod = "POST"
         return execute(request).eraseToAnyPublisher()
@@ -92,14 +94,14 @@ public extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `PUT` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func put(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func put(_ url: URL) -> AnyPublisher<HTTPResponse, HTTPError> {
         let request = newRequest(url)
         return post(request)
     }
 
     /// Execute a `PUT` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func put(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func put(_ request: URLRequest) -> AnyPublisher<HTTPResponse, HTTPError> {
         var request = request.appending(headers: customHeaders)
         request.httpMethod = "PUT"
         return execute(request).eraseToAnyPublisher()
@@ -111,14 +113,14 @@ public extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `DELETE` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func delete(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func delete(_ url: URL) -> AnyPublisher<HTTPResponse, HTTPError> {
         let request = newRequest(url)
         return post(request)
     }
 
     /// Execute a `DELETE` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func delete(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
+    func delete(_ request: URLRequest) -> AnyPublisher<HTTPResponse, HTTPError> {
         var request = request.appending(headers: customHeaders)
         request.httpMethod = "DELETE"
         return execute(request).eraseToAnyPublisher()
