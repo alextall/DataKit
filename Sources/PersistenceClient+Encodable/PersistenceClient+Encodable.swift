1fcfd06a9b592d5d840e5c21c73b5100e71f659b
Alex <alextall@users.noreply.github.com>
2019-11-27 19:23:31 -0600

    Add support for injecting Encoders

--- Sources/PersistenceClient+Encodable/PersistenceClient+Encodable.swift
+++ Sources/PersistenceClient+Encodable/PersistenceClient+Encodable.swift
@@ -4,9 +4,9 @@ import PersistenceClient
 
 public extension PersistenceClient {
     /// Encodes an instance of the indicated type.
-    func encodeToJSON<T: NSManagedObject>(scratchPad: ScratchPad<T>) throws -> Data where T: Encodable {
-        let encoder = JSONEncoder()
-
+    func encodeToJSON<T: NSManagedObject>(scratchPad: ScratchPad<T>,
+                                          with encoder: JSONEncoder = JSONEncoder()) throws 
+        -> Data where T: Encodable {
         switch scratchPad {
         case .empty: throw PersistenceError.encoding
         case .list(value: let list, context: _): return try encoder.encode(list)
@@ -17,9 +17,9 @@ public extension PersistenceClient {
 
 public extension PersistenceClient {
     /// Encodes an instance of the indicated type.
-    func encodeToPlist<T: NSManagedObject>(scratchPad: ScratchPad<T>) throws -> Data where T: Encodable {
-        let encoder = PropertyListEncoder()
-
+    func encodeToPlist<T: NSManagedObject>(scratchPad: ScratchPad<T>,
+                                           with encoder: PropertyListEncoder = PropertyListEncoder()) throws 
+    -> Data where T: Encodable {
         switch scratchPad {
         case .empty: throw PersistenceError.encoding
         case .list(value: let list, context: _): return try encoder.encode(list)
