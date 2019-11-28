9ade500b06b8e2c846d749f9e7f8f652306414ba
Alex <alextall@users.noreply.github.com>
2019-11-27 19:20:28 -0600

    Allow injecting decoder

--- Sources/PersistenceClient+Decodable/PersistenceClient+Decodable.swift
+++ Sources/PersistenceClient+Decodable/PersistenceClient+Decodable.swift
@@ -5,27 +5,27 @@ import PersistenceClient
 public extension PersistenceClient {
     /// Decodes an instance of the indicated type.
     func decodeJSON<T: NSManagedObject>(_ data: Data,
+                                        with decoder: JSONDecoder = JSONDecoder(),
                                         to _: T.Type,
                                         in context: NSManagedObjectContext? = nil) throws
         -> ScratchPad<T> where T: Decodable {
-        let decoder = JSONDecoder()
         let context = context ?? newBackgroundContext()
         decoder.userInfo[CodingUserInfoKey.managedObjectContext!] = context
-
+        
         let result = try decoder.decode(T.self, from: data)
-
+        
         return .object(value: result, context: context)
     }
-
+    
     /// Decodes an instance of the indicated type.
     func decodeJSON<T: NSManagedObject>(_ data: Data,
+                                        with decoder: JSONDecoder = JSONDecoder(),
                                         to _: [T].Type,
                                         in context: NSManagedObjectContext? = nil) throws
         -> ScratchPad<T> where T: Decodable {
-        let decoder = JSONDecoder()
         let context = context ?? newBackgroundContext()
         decoder.userInfo[CodingUserInfoKey.managedObjectContext!] = context
-
+        
         do {
             let result = try decoder.decode([T].self, from: data)
             return .list(value: result, context: context)
@@ -38,13 +38,13 @@ public extension PersistenceClient {
 public extension PersistenceClient {
     /// Decodes an instance of the indicated type.
     func decodePlist<T: NSManagedObject>(_ data: Data,
+                                         with decoder: PropertyListDecoder = PropertyListDecoder(),
                                          to _: T.Type,
                                          in context: NSManagedObjectContext? = nil) throws
         -> ScratchPad<T> where T: Decodable {
-        let decoder = PropertyListDecoder()
         let context = context ?? newBackgroundContext()
         decoder.userInfo[CodingUserInfoKey.managedObjectContext!] = context
-
+        
         do {
             let result = try decoder.decode([T].self, from: data)
             return .list(value: result, context: context)
@@ -52,16 +52,16 @@ public extension PersistenceClient {
             throw PersistenceError.decoding
         }
     }
-
+    
     /// Decodes an instance of the indicated type.
     func decodePlist<T: NSManagedObject>(_ data: Data,
+                                         with decoder: PropertyListDecoder = PropertyListDecoder(),
                                          to _: [T].Type,
                                          in context: NSManagedObjectContext? = nil) throws
         -> ScratchPad<T> where T: Decodable {
-        let decoder = PropertyListDecoder()
         let context = context ?? newBackgroundContext()
         decoder.userInfo[CodingUserInfoKey.managedObjectContext!] = context
-
+        
         do {
             let result = try decoder.decode([T].self, from: data)
             return .list(value: result, context: context)
