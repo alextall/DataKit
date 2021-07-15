116c44b54e0ea595d8758a83f2624b5d4f709129
Alex Du Bois <alex.dubois@mac.com>
2021-07-15 09:37:55 -0500

    Type erase HTTPClient
    Signed-off-by: Alex Du Bois <alex.dubois@mac.com>

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -22,7 +22,7 @@ public enum HTTPError: Error {
 extension HTTPClient {
     /// Execute the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func execute(_ request: URLRequest) -> Future<HTTPOutput, HTTPError> {
+    func execute(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
         return Future { promise in
             self.session.dataTask(with: request,
                                   completionHandler: { data, response, error in
@@ -40,6 +40,7 @@ extension HTTPClient {
                                     promise(.success(.empty(response)))
             }).resume()
         }
+        .eraseToAnyPublisher()
     }
 }
 
@@ -48,21 +49,21 @@ extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `GET` HTTP request at the given endpoint
     /// - Paramenter url: A `String` to be appended to the base URL
-    func get(_ pathComponents: String) -> Future<HTTPOutput, HTTPError> {
+    func get(_ pathComponents: String) -> AnyPublisher<HTTPOutput, HTTPError> {
         let url = baseURL.appendingPathComponent(pathComponents)
         return get(url)
     }
     
     /// Execute a `GET` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func get(_ url: URL) -> Future<HTTPOutput, HTTPError> {
+    func get(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
         let request = newRequest(url)
         return get(request)
     }
     
     /// Execute a `GET` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func get(_ request: URLRequest) -> Future<HTTPOutput, HTTPError> {
+    func get(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
         var request = request
         request.httpMethod = "GET"
         return execute(request)
@@ -74,21 +75,21 @@ public extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `POST` HTTP request at the given endpoint
     /// - Paramenter url: A `String` to be appended to the base URL
-    func post(_ pathComponents: String) -> Future<HTTPOutput, HTTPError> {
+    func post(_ pathComponents: String) -> AnyPublisher<HTTPOutput, HTTPError> {
         let url = baseURL.appendingPathComponent(pathComponents)
         return post(url)
     }
     
     /// Execute a `POST` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func post(_ url: URL) -> Future<HTTPOutput, HTTPError> {
+    func post(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
         let request = newRequest(url)
         return post(request)
     }
     
     /// Execute a `POST` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func post(_ request: URLRequest) -> Future<HTTPOutput, HTTPError> {
+    func post(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
         var request = request
         request.httpMethod = "POST"
         return execute(request)
@@ -100,21 +101,21 @@ public extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `PUT` HTTP request at the given endpoint
     /// - Paramenter url: A `String` to be appended to the base URL
-    func put(_ pathComponents: String) -> Future<HTTPOutput, HTTPError> {
+    func put(_ pathComponents: String) -> AnyPublisher<HTTPOutput, HTTPError> {
         let url = baseURL.appendingPathComponent(pathComponents)
         return put(url)
     }
     
     /// Execute a `PUT` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func put(_ url: URL) -> Future<HTTPOutput, HTTPError> {
+    func put(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
         let request = newRequest(url)
         return post(request)
     }
     
     /// Execute a `PUT` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func put(_ request: URLRequest) -> Future<HTTPOutput, HTTPError> {
+    func put(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
         var request = request
         request.httpMethod = "PUT"
         return execute(request)
@@ -126,21 +127,21 @@ public extension HTTPClient {
 public extension HTTPClient {
     /// Execute a `DELETE` HTTP request at the given endpoint
     /// - Paramenter url: A `String` to be appended to the base URL
-    func delete(_ pathComponents: String) -> Future<HTTPOutput, HTTPError> {
+    func delete(_ pathComponents: String) -> AnyPublisher<HTTPOutput, HTTPError> {
         let url = baseURL.appendingPathComponent(pathComponents)
         return delete(url)
     }
     
     /// Execute a `DELETE` HTTP request with the provided `URL`
     /// - Parameter url: A `URL` object to use for the request
-    func delete(_ url: URL) -> Future<HTTPOutput, HTTPError> {
+    func delete(_ url: URL) -> AnyPublisher<HTTPOutput, HTTPError> {
         let request = newRequest(url)
         return post(request)
     }
     
     /// Execute a `DELETE` HTTP request with the provided `URLRequest`
     /// - Parameter request: A `URLRequest` to execute
-    func delete(_ request: URLRequest) -> Future<HTTPOutput, HTTPError> {
+    func delete(_ request: URLRequest) -> AnyPublisher<HTTPOutput, HTTPError> {
         var request = request
         request.httpMethod = "DELETE"
         return execute(request)
